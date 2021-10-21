fff3d0bad1793aa2576c57469b0380ffb143d59e
hoyipyik <hoyipyik@protonmail.com>
2021-10-21 14:15:00 +0800

    sidebar-menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,9 +1,30 @@
 import React from 'react'
 
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+
+import "./Menu.css"
+
 const  Menu = (props) =>{
     return (
-        <div>
-            This is Menu
+        <div className='Menu'>
+            <div className='menu-item'>
+                <span><SubscriptionsIcon/></span>
+                <span className='label'>Channels</span>
+            </div>
+            
+            <div className='menu-item'>
+                <span><SearchIcon/></span>
+                <span className='label'>Search</span>
+            </div>
+            
+            <div className='menu-item'>
+                <span><VideoLibraryIcon/></span>
+                <span className='label'>Collections</span>
+            </div>
+            
         </div>
     )
 }
