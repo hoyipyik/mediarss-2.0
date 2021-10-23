80f36d54fabbec51c0c05b04d366a2be5f561d26
hoyipyik <hoyipyik@protonmail.com>
2021-10-23 11:53:23 +0800

    update image

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import "./Home.css"
+import poster from "../../poster.jpeg"
 
 export default class Home extends Component {
     render() {
@@ -8,7 +9,7 @@ export default class Home extends Component {
         const channelItem = 
             <div className='channel-item'>
                 <figure>
-                    <img src='https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fencrypted-tbn0.gstatic.com%2Fimages%3Fq%3Dtbn%3AANd9GcT1YLzPewVnZ_uYNyyZooUeUP5PJMemi1tcpDNUQ1O-0GNV4k8O%26s&sp=1634803883T4594da3458a58732a823cf29bb470b470d1689d86cc0f6a9d7fa686bc224b62c'/>
+                    <img src={poster}/>
                     <figcaption>Hi Spotify</figcaption>
                 </figure>
             </div>
