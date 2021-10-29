e9500ed69f4a3c2b3909656c2e7c42b6b445eb4e
hoyipyik <hoyipyik@protonmail.com>
2021-10-29 22:15:18 +0800

    fix pinned bugs

--- src/App.js
+++ src/App.js
@@ -99,7 +99,6 @@ class App extends Component {
   }
 
   homeHandler = () =>{
-    // const {channelItemPageFlag, homeFlag} = this.state
     this.setState({
       channelItemPageFlag: false,
       homeFlag: true,
