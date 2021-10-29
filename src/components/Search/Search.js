63d7b5abcf3fbcee0f82077dd116bcc1f9041f61
hoyipyik <hoyipyik@protonmail.com>
2021-10-29 10:12:31 +0800

    finish search bar

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
 
+import SearchIcon from '@material-ui/icons/Search';
+import "./Search.css"
+
 export default class Search extends Component {
     render() {
         return (
             <div className='Search'>
-                <h1>This is Search Page</h1>
+                <div className='container'>
+                    <section className="search">    
+                        <h1>Search</h1>
+                        <input />
+                        <span><SearchIcon /></span>
+                    </section>
+
+                    <section className='contents'>
+
+                    </section>
+                </div>
             </div>
         )
     }
